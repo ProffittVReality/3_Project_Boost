8ccc93415ff86f4991fc453c60e7070e210f67f4
ProffittVReality <uvaperceptionlab@gmail.com>
2018-10-25 16:01:34 -0400

    changed controlls
    made the turning and thrusting speeds public for easy editing; added tags to lable safe and dangerous things

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 75f; //public brings to inspector, but makes it changeable from other scripts
+    [SerializeField] float thrustPower = 10f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -16,31 +19,62 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
-    { 
+    private void Thrust()
+    {
+
+        //float thrustThisFrame = thrustPower * Time.deltaTime; //this time delta time makes it frame rate independent 
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up); //relative to its orientation
+            rigidBody.AddRelativeForce(Vector3.up * thrustPower); //relative to its orientation
             if (!audioSource.isPlaying)//if not playing is the exclamation point
-            audioSource.Play();
+                audioSource.Play();
         }
         else
         {
             audioSource.Stop();
         }
 
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // before take manual controll of rotation, freeze it so it doesn't spin out of controll
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime; //this time delta time makes it frame rate independent 
+
         if (Input.GetKey(KeyCode.A)) //the double if statements here lets it thrust and turn at the same time.
         {
-           transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
         else if (Input.GetKey(KeyCode.D)) //Only A and D are mutually exclusive
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
+        rigidBody.freezeRotation = false; // physics engine takes over when you aren't pressing buttons
+
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        { 
+            case "Friendly":
+            print("OK");
+            break;
+        //case "Fuel":
+            //print("Fuel");
+            //break;
+        default:
+            print("DEAD!!");
+            break;
+        }
+    }
+
 }
